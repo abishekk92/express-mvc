d6bd5f5f6d10923576295b50da1b87916dadeb86
Abishek Bhat <bhat.abishek@gmail.com>
2012-02-02 17:07:37 +0530

    Coffeescript support added

--- express.sh
+++ express.sh
@@ -3,7 +3,11 @@ echo "created express app at:"$1
 cd $1
 echo "Installing dependencies"
 npm install -l
+npm install js2coffee
 mkdir models
+js2coffee app.js > app.coffee
+cd routes
+js2coffee index.js > index.coffee
 
 echo "Express MVC App Set up"
 
--- foobar/routes/index.coffee
+++
@@ -1,8 +0,0 @@
-Post=require "../models/post.js"
-exports.post=(req,res)->
-	new Post(
-		title:req.body.title
-		author:req.body.author
-		content:req.body.content
-                ).save()
-
